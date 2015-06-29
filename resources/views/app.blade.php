db6d58adfe028c6f8374aefda3fc61668417eeb9
Steven Paquet <steven.paquet1@gmail.com>
2015-06-29 10:44:20 -0400

    second commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,9 +2,10 @@
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
-        <title>Document</title>
+        <title>Tracker</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <link rel="stylesheet" href="{{ elixir('css/all.css') }}">
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css" rel="stylesheet" />
     </head>
     <body>
         <div class="container">
@@ -13,13 +14,13 @@
             @yield('content') 
         </div>
 
-        <script src="//code.jquery.com/jquery.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
         <script>
             $('div.alert').not('.alert-important').delay(3000).slideUp(300);
         </script>
         
+        <script src="//code.jquery.com/jquery.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script>
         @yield('footer')
     </body>
 </html>
