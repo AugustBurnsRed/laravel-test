32ca9df1f8dd1840f67ab976cff35470e23b2265
Steven Paquet <steven.paquet1@gmail.com>
2015-06-30 13:41:30 -0400

    test

--- resources/views/torrents/index.blade.php
+++ resources/views/torrents/index.blade.php
@@ -8,7 +8,6 @@
             <h2>
                 <a href="{{ action('SeriesController@showEpisode', ['toDoNomSerie', $torrent->id]) }}">{{ $torrent->title }}</a>
             </h2>
-
             <div class="description">{{ $torrent->description }} <br> {{ $torrent->created_at->diffForHumans() }}</div>
         </article>
     @endforeach
